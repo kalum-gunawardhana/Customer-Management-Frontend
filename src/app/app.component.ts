d1dff61e2bc7c00406b3df6e0c0475dad3936fac
Kalum Gunawardhana <kguna415@gmail.com>
2025-04-30 21:38:41 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabBarComponent } from "./component/tab-bar/tab-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/component/tab-bar/tab-bar.component.ts
+++ src/app/component/tab-bar/tab-bar.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import { MatTabsModule } from '@angular/material/tabs';
+import { AsyncPipe } from '@angular/common';
+
+export interface TabBar {
+  label: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-tab-bar',
-  imports: [],
+  imports: [MatTabsModule, AsyncPipe],
   templateUrl: './tab-bar.component.html',
   styleUrl: './tab-bar.component.css'
 })
 export class TabBarComponent {
+  asyncTabs: Observable<TabBar[]>;
 
+  constructor() {
+    this.asyncTabs = new Observable((observer: Observer<TabBar[]>) => {
+      setTimeout(() => {
+        observer.next([
+          { label: 'First', content: 'Content 1' },
+          { label: 'Second', content: 'Content 2' },
+          { label: 'Third', content: 'Content 3' },
+        ]);
+      }, 1000);
+    });
+  }
 }
