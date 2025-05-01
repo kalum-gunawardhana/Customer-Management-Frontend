66d4947876e5f89c125a640ce0b7394f86cbea65
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-01 10:57:56 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ViewCusComponent } from './component/view-cus/view-cus.component';
 import { TabBarComponent } from './component/tab-bar/tab-bar.component';
 import { AddCusComponent } from './component/add-cus/add-cus.component';
 import { UpdateCusComponent } from './component/update-cus/update-cus.component';
 import { SearchCusComponent } from './component/search-cus/search-cus.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    // {path:'', component:TabBarComponent},
-    {path:'view-cus', component:ViewCusComponent},
-    {path:'add-cus', component:AddCusComponent},
-    {path:'update-cus', component:UpdateCusComponent},
-    {path:'search-cus', component:SearchCusComponent}
+    {
+        path: '', component: TabBarComponent, children: [
+            { path: 'add-cus', component: AddCusComponent },
+            { path: 'search-cus', component: SearchCusComponent },
+            { path: 'update-cus', component: UpdateCusComponent },
+            { path: 'view-cus', component: ViewCusComponent },
+        ]
+    }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
